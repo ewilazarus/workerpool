6b061d6edf2ee49aef7be91766fd5d03d9280c60
Gabriel Lima <gvclima@gmail.com>
2018-11-22 18:37:54 +0000

    First commit

---
+++ sample.py
@@ -0,0 +1,14 @@
+from workerpool import IOWorkerPool
+import time
+import random
+
+def consumer(i):
+    time.sleep(random.random())
+    print(i)
+
+if __name__ == '__main__':
+    with IOWorkerPool() as pool:
+        while True:
+            for i in range(100):
+                pool.submit(consumer, i)
+            time.sleep(15)
